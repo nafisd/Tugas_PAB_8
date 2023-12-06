444cf2b6d8374ab0b76f1bf5e65901a34823f073
nafis d <nafisiarnoldi34@gmail.com>
2023-12-06 07:35:51 +0700

    first commit

---
+++ src/components/kecil/index.js
@@ -0,0 +1,8 @@
+
+import Button from './button' 
+import Input from './input'
+import CategoryTab from './categoryTab'
+import Pilihan from './pilihan'
+
+export {Button, Input, CategoryTab, Pilihan}
+
